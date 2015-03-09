98e08e7d873aa3f8f6ab79c573f1d5c31ea7ee51
Zvi "Viz" Effron <viz@flippedperspective.com>
2015-03-09 03:59:59 -0700

    Fixup module to be idiomatic and compile with cargo

---
+++ build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("cargo:rustc-flags=-L C:/Progra~2/Micros~2.0/VC/lib/store/amd64");
+}
---
+++ src/platform/details/mod.rs
@@ -0,0 +1,31 @@
+// For reference on linking to C++ functions, see https://github.com/rust-lang/rust/issues/5853#issuecomment-44365672
+
+#[cfg(target_family = "windows")]
+#[allow(non_snake_case)]
+mod Platform {
+    pub mod Details {
+        #[link(name = "vccorlib")]
+        extern "C" {
+            // This generates invalid program output causing a crash when this function is called. See https://github.com/rust-lang/rust/issues/23216 for details.
+            #[link_name = "?GetCmdArguments@Details@Platform@@YAPEAPEA_WPEAH@Z"]
+            pub fn GetCmdArguments(argc : *mut ::libc::c_int) -> *const *const ::libc::wchar_t;
+        }
+    }
+}
+
+#[unstable]
+pub fn get_command_arguments(argc : ::libc::c_int) -> (*const *const ::libc::wchar_t, ::libc::c_int) {
+    let mut argc = argc;
+    ( unsafe { Platform::Details::GetCmdArguments(&mut argc) }, argc)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_command_arguments() {
+        let (argv, argc) = get_command_arguments(0);
+        assert!(argc == 1);
+    }
+}
