d085dca9f96feeced796fee7a3d9d9581542bdab
Zvi "CtrlZvi" Effron <viz+github@flippedperspective.com>
2015-08-01 16:41:46 -0700

    Make get_command_arguments slightly more Rust friendly

--- src/platform/details/mod.rs
+++ src/platform/details/mod.rs
@@ -22,9 +22,10 @@ mod Platform {
     }
 }
 
-pub fn get_command_arguments(argc : ::libc::c_int) -> (*const *const ::libc::wchar_t, ::libc::c_int) {
+pub fn get_command_arguments(argc : ::libc::c_int) -> (&'static [* const ::libc::wchar_t], usize) {
     let mut argc = argc;
-    ( unsafe { Platform::Details::GetCmdArguments(&mut argc) }, argc)
+    let argv = unsafe { Platform::Details::GetCmdArguments(&mut argc) };
+    ( unsafe { ::std::slice::from_raw_parts(argv, argc as usize) }, argc as usize)
 }
 
 #[cfg(test)]
@@ -35,5 +36,10 @@ mod tests {
     fn test_get_command_arguments() {
         let (argv, argc) = get_command_arguments(0);
         assert!(argc == 1);
+        for i in 0..argc {
+            let argument : &[::libc::wchar_t] = unsafe { ::std::slice::from_raw_parts(argv[i], ::libc::wcslen(argv[i]) as usize) };
+            // println!("Foo: {}", argument)
+            // assert!(argument[0] == 'C' as u16)
+        }
     }
 }
